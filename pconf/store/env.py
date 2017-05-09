0024f70029efb9edfbac4e6152d119fed08936e3
András Maróy <andras@maroy.hu>
2017-05-09 18:57:04 +0200

    Implement environment variable parsing

--- pconf/store/env.py
+++ pconf/store/env.py
@@ -1,12 +1,53 @@
 import os
+import re
 
 
 class Env(object):
-    def __init__(self, separator=':', match=None, whitelist=None):
+    def __init__(self, separator=None, match=None, whitelist=None):
         self.separator = separator
         self.match = match
         self.whitelist = whitelist
 
+        if self.match is not None:
+            self.re = re.compile(self.match)
+
     def get(self):
+        results = {}
         env_vars = os.environ
-        return env_vars
+
+        for key in env_vars.keys():
+            if self.__valid_key(key):
+                results[key] = env_vars[key]
+
+        if self.separator is not None:
+            self.__split_vars(results)
+
+        return results
+
+    def __valid_key(self, key):
+        if self.match is not None and self.whitelist is not None:
+            return key in self.whitelist or self.re.search(key) is not None
+        elif self.match is not None:
+            return self.re.search(key) is not None
+        elif self.whitelist is not None:
+            return key in self.whitelist
+        else:
+            return True
+
+    def __split_vars(self, env_vars):
+        for key in env_vars.keys():
+            splits = key.split(self.separator)
+            splits = filter(None, splits)
+
+            if len(splits) != 1:
+                split = self.__split_var(splits, env_vars[key])
+                del env_vars[key]
+                env_vars[split.keys()[0]] = split.values()[0]
+
+    def __split_var(self, keys, value):
+        if len(keys) == 1:
+            return {keys[0]: value}
+        else:
+            key = keys[0]
+            del keys[0]
+            return {key: self.__split_var(keys, value)}
--- tests/test_env.py
+++ tests/test_env.py
@@ -4,18 +4,24 @@ import pconf
 from pconf.store.env import Env
 
 
-TEST_ENV_VARS = {'env': 'result'}
+TEST_ENV_BASE_VARS = {'env__var': 'result', }
+TEST_ENV_MATCHED_VARS = {'matched_var': 'match'}
+TEST_ENV_WHITELIST_VARS = {'whitelisted_var': 'whitelist'}
+TEST_SEPARATED_VARS = {'env': {'var': 'result'}}
+TEST_ENV_VARS = dict(TEST_ENV_WHITELIST_VARS, **TEST_ENV_MATCHED_VARS)
+TEST_SEPARATED_VARS = dict(TEST_SEPARATED_VARS, **TEST_ENV_VARS)
+TEST_ENV_VARS = dict(TEST_ENV_VARS, **TEST_ENV_BASE_VARS)
+
 TEST_SEPARATOR = '__'
-TEST_MATCH = 'match'
-TEST_WHITELIST = ['whitelist1', 'whitelist2']
+TEST_MATCH = r'^matched'
+TEST_WHITELIST = ['whitelisted_var', 'whitelist2']
 
 
-@patch('pconf.store.env.os', new=MagicMock())
 class TestEnv(TestCase):
     def test_default_params(self):
         env_store = Env()
 
-        self.assertEqual(env_store.separator, ':')
+        self.assertEqual(env_store.separator, None)
         self.assertEqual(env_store.match, None)
         self.assertEqual(env_store.whitelist, None)
 
@@ -26,6 +32,7 @@ class TestEnv(TestCase):
         self.assertEqual(env_store.match, TEST_MATCH)
         self.assertEqual(env_store.whitelist, TEST_WHITELIST)
 
+    @patch('pconf.store.env.os', new=MagicMock())
     def test_get_all_vars(self):
         pconf.store.env.os.environ = TEST_ENV_VARS
 
@@ -33,4 +40,40 @@ class TestEnv(TestCase):
         result = env_store.get()
 
         self.assertEqual(result, TEST_ENV_VARS)
+        self.assertIsInstance(result, dict)
+
+    @patch('pconf.store.env.os', new=MagicMock())
+    def test_whitelist(self):
+        pconf.store.env.os.environ = TEST_ENV_VARS
+        env_store = Env(whitelist=TEST_WHITELIST)
+        result = env_store.get()
+
+        self.assertEqual(result, TEST_ENV_WHITELIST_VARS)
+        self.assertIsInstance(result, dict)
+
+    @patch('pconf.store.env.os', new=MagicMock())
+    def test_match(self):
+        pconf.store.env.os.environ = TEST_ENV_VARS
+        env_store = Env(match=TEST_MATCH)
+        result = env_store.get()
+
+        self.assertEqual(result, TEST_ENV_MATCHED_VARS)
+        self.assertIsInstance(result, dict)
+
+    @patch('pconf.store.env.os', new=MagicMock())
+    def test_whitelist_and_match(self):
+        pconf.store.env.os.environ = TEST_ENV_VARS
+        env_store = Env(match=TEST_MATCH, whitelist=TEST_WHITELIST)
+        result = env_store.get()
+
+        self.assertEqual(result, dict(TEST_ENV_MATCHED_VARS, **TEST_ENV_WHITELIST_VARS))
+        self.assertIsInstance(result, dict)
+
+    @patch('pconf.store.env.os', new=MagicMock())
+    def test_separator(self):
+        pconf.store.env.os.environ = TEST_ENV_VARS
+        env_store = Env(separator=TEST_SEPARATOR)
+        result = env_store.get()
+
+        self.assertEqual(result, TEST_SEPARATED_VARS)
         self.assertIsInstance(result, dict)
