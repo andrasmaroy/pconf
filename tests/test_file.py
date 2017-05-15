224ba75113c0cff1f08a04bfb7d9dc099ef7925c
András Maróy <andras@maroy.hu>
2017-05-15 15:51:28 +0200

    Fix tests on travis (possibly)

--- tests/test_file.py
+++ tests/test_file.py
@@ -15,13 +15,13 @@ def throw_ioerror(*args, **kwargs):
 
 
 class TestFile(TestCase):
-    @patch('pconf.store.file.open')
+    @patch('__builtin__.open')
     def test_open_right_file(self, mock_open):
         File(TEST_FILE_PATH)
 
         mock_open.assert_called_once_with(TEST_FILE_PATH, 'r')
 
-    @patch('pconf.store.file.open', side_effect=throw_ioerror)
+    @patch('__builtin__.open', side_effect=throw_ioerror)
     def test_open_nonexistent_file_returns_empty_dict(self, mock_open):
         file_store = File(TEST_FILE_PATH)
         result = file_store.get()
@@ -29,7 +29,7 @@ class TestFile(TestCase):
         self.assertEqual(result, {})
         self.assertIsInstance(result, dict)
 
-    @patch('pconf.store.file.open', side_effect=throw_ioerror)
+    @patch('__builtin__.open'.format(__name__), side_effect=throw_ioerror)
     def test_custom_encoding_without_parser_returns_empty_dict(self, mock_open):
         file_store = File(TEST_FILE_PATH, encoding='foo')
         result = file_store.get()
@@ -37,7 +37,7 @@ class TestFile(TestCase):
         self.assertEqual(result, {})
         self.assertIsInstance(result, dict)
 
-    @patch('pconf.store.file.open', mock_open(read_data=TEST_FILE_RAW))
+    @patch('__builtin__.open', mock_open(read_data=TEST_FILE_RAW))
     def test_get_raw(self):
         file_store = File(TEST_FILE_PATH)
         result = file_store.get()
@@ -45,7 +45,7 @@ class TestFile(TestCase):
         self.assertEqual(result, TEST_FILE_DICT)
         self.assertIsInstance(result, dict)
 
-    @patch('pconf.store.file.open', mock_open(read_data=TEST_FILE_RAW))
+    @patch('__builtin__.open', mock_open(read_data=TEST_FILE_RAW))
     def test_get_idempotent(self):
         file_store = File(TEST_FILE_RAW)
         result = file_store.get()
@@ -59,7 +59,7 @@ class TestFile(TestCase):
         self.assertIsInstance(result, dict)
         pconf.store.file.open.assert_called_once()
 
-    @patch('pconf.store.file.open', mock_open(read_data=TEST_FILE_RAW))
+    @patch('__builtin__.open', mock_open(read_data=TEST_FILE_RAW))
     def test_get_custom_encoding(self):
         mock_parser = MagicMock()
         mock_parser.return_value = TEST_FILE_DICT
@@ -70,7 +70,7 @@ class TestFile(TestCase):
         self.assertEqual(result, TEST_FILE_DICT)
         self.assertIsInstance(result, dict)
 
-    @patch('pconf.store.file.open', mock_open(read_data=TEST_FILE_JSON))
+    @patch('__builtin__.open', mock_open(read_data=TEST_FILE_JSON))
     def test_get_json(self):
         file_store = File(TEST_FILE_PATH, encoding='json')
         result = file_store.get()
