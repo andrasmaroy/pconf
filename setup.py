867cf3395bdd6b26b87142119f21a2880b014367
András Maróy <andras@maroy.hu>
2023-10-11 10:58:01 +0200

    Add support for Python 3.12

--- setup.py
+++ setup.py
@@ -32,7 +32,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11'
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
     ],
     install_requires=['pyyaml', 'deepmerge'],
     extras_require={
